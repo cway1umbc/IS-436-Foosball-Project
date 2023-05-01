bbf089763cf5b46800d00cb3960fac56f0e2ea96
Yunaix <euniceantwi719@gmail.com>
2023-05-01 18:35:42 -0400

    com

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ $dbname = "eunicea2";
 			// Insert into database
 			$sql = "INSERT INTO  `signup` ( `fname`,  `lname`,  `email`,  `password`) VALUES('$fname', '$lname', '$email', '$password')";
 			
+			
 			// connection sucess check
 			$query = mysqli_query($conn,$sql);
 			if($query){
@@ -36,3 +37,16 @@ $dbname = "eunicea2";
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+<button>Continue<a href= "index2.html"></button>
+</body>
+</html>
