fd4cdcc5866f1af9958e01ab5baa81d1ca042bdf
Yunaix <euniceantwi719@gmail.com>
2023-05-08 16:34:26 -0400

    commit

--- retriever_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) 
-{
-    echo "<H3>Welcome to the user's area, user: " . $_SESSION['email'] . "!</H3>";
-} else {
-    header("Location: index.html");
-}
-?>
