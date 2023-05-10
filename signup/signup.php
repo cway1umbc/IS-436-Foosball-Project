c82511a92f284c62d64237748409205ea8ff12fe
Yunaix <euniceantwi719@gmail.com>
2023-05-10 19:27:31 -0400

    m

--- signup/signup.php
+++ signup/signup.php
@@ -1,6 +1,5 @@
 <?php
 
-// database connection
 $servername = "studentdb-maria.gl.umbc.edu";
 $username = "eunicea2";
 $password = "eunicea2";
@@ -8,6 +7,7 @@ $dbname = "eunicea2";
 
 
 	if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
+
 		$conn = mysqli_connect($servername, $username, $password, $dbname)
 		or die("Connection Failed:" .mysqli_connect_error());
 		
@@ -26,7 +26,7 @@ $dbname = "eunicea2";
 			// connection sucess check
 			$query = mysqli_query($conn,$sql);
 			if($query){
-				echo 'Entry Sucessful';
+				header("Location: login.html")
 			} else {
 				echo 'Error Occured';
 			}
@@ -38,15 +38,3 @@ $dbname = "eunicea2";
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body>
-<button>Continue<a href= "index2.html"></button>
-</body>
-</html>
