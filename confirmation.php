4f1b103cc65fd14decc94778747b94ea614f81bf
Yunaix <euniceantwi719@gmail.com>
2023-05-14 17:39:59 -0400

    ok

---
+++ confirmation.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Confirmation Page</title>
+</head>
+<body>
+	<h1>Signup Successful!</h1>
+	<p>Thank you for signing up. Your account has been created successfully.</p>
+	<p>Please proceed to <a href="login.html">login page</a> to login.</p>
+</body>
+</html>
